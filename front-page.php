1b0aacab7ce9dae8e297748861b9be5871738299
Tung Thanh <cs.tungthanh@gmail.com>
2021-11-27 16:32:18 +0700

    add popular product

--- front-page.php
+++ front-page.php
@@ -16,7 +16,8 @@ get_header();
 ?>
 <main id="primary" class="site-main">
     <section class="container pb-5 pt-4">
-        <div id="carouselExampleDark" class="carousel carousel-dark slide overflow-hidden rounded" data-bs-ride="carousel">
+        <div id="carouselExampleDark" class="carousel carousel-dark slide overflow-hidden rounded"
+            data-bs-ride="carousel">
             <div class="carousel-indicators">
                 <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active"
                     aria-current="true" aria-label="Slide 1"></button>
@@ -25,10 +26,16 @@ get_header();
             </div>
             <div class="carousel-inner">
                 <div class="carousel-item active" data-bs-interval="10000">
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/slider/slide-1.jpg" class="d-block w-100" alt="...">
+                    <a href="#">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/slider/slide-1.jpg"
+                            class="d-block w-100" alt="...">
+                    </a>
                 </div>
                 <div class="carousel-item" data-bs-interval="2000">
-					<img src="<?php echo get_template_directory_uri(); ?>/img/slider/slide-2.jpg" class="d-block w-100" alt="...">
+                    <a href="#">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/slider/slide-2.jpg"
+                            class="d-block w-100" alt="...">
+                    </a>
                 </div>
             </div>
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark"
@@ -44,6 +51,16 @@ get_header();
         </div>
     </section>
 
+    <section class="container">
+        <h1 class="text-center pt-5">Popular Products</h1>
+        <p class="text-center">
+            We offer a number of high quality toys to help keep<br> your pet healthy and spoiled!
+        </p>
+
+		<div class="pt-5 pb-5">
+			<?php echo do_shortcode( '[products columns=4 limit=4]' ); ?>
+		</div>
+    </section>
 
 </main>
 
